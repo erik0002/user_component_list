3beb2b9437944a301ff99e4f2c368e589e12cdfa
Yeliazar <erik.ruzhakov@mail.ru>
2022-08-16 16:30:42 +0300

    add userState Management

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 
 const AddUser = props => {
+
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredUserAge, setEnteredUserAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredUsername, enteredUserAge);
+        setEnteredUsername('');
+        setEnteredUserAge('');
+    }
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value)
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredUserAge(event.target.value)
     }
 
        return (
            <Card cssName={classes.input}>
                <form onSubmit={addUserHandler}>
                    <label htmlFor="username">Username</label>
-                   <input id="username" type="text"/>
+                   <input id="username" type="text" onChange={usernameChangeHandler}/>
                    <label htmlFor="age">Age (Years)</label>
-                   <input id="age" type="number"/>
+                   <input id="age" type="number" onChange={ageChangeHandler}/>
                    <Button type="submit">Add User</Button>
                </form>
            </Card>
